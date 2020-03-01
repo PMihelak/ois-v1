6298ec29ca723e1ca4c73fae856a81857e1173d9
mitici <ma6958@student.uni-lj.si>
2020-03-01 01:16:59 +0000

    Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,9 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.print("OIS je zakon!");
     }
+    public static void (nadmorskaVisina, gravitacijskiPospesek) {
+        
+        System.out.println(nadmorskaVisina, gravitacijskiPospesek);
+        
+    }    
 }
